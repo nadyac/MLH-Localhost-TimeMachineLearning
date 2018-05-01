1bd083e3a15af6c06e55b719bd98d295bb8663b6
nadyac <nadyacpena@gmail.com>
2018-05-01 08:28:58 -0400

    Adding app and images. App uses modified code from Nexosis's Big Foot sighting classinator.

---
+++ site/app.js
@@ -0,0 +1,77 @@
+//const classinateUrl = 'https://rccptdhzsa.execute-api.us-east-1.amazonaws.com/prod/bigfootClassinator';
+const classinateUrl = 'https://ml.nexosis.com/v1/models/6dc983e1-1b23-462d-b9a5-74661c54fb71/predict'
+const classMessages = {
+    'Marty': "Marty!",
+    'Doc': "Doc!",
+};
+
+const waitMessage = "Analyzing your quote...";
+const errorMessage = "There was an error processing your quote =(.";
+
+var length = function(quote){
+    for (i = 0; i <= quote.length; i++){
+        ;
+    }
+    return i;
+}
+
+var punctuation = function(quote){
+    var punctuation = 0;
+    for (i = 0; i < quote.length; i++){
+        if(quote[i] != '"' && quote[i] === '!' || quote[i] === '?' || quote[i] ==='...'){
+            punctuation++;
+        }; 
+    }
+    return punctuation;
+}
+
+var caps = function(quote){
+    var caps = 0;
+    for (i = 0; i < quote.length; i++){
+            if(quote[i] === quote[i].toUpperCase()){
+                caps++;
+            }
+    }
+    return caps;
+}
+
+var longestWord = function(quote){
+    var words = quote.split(" ")
+    var longest = 0;
+    for (var i= 0; i < words.length; i++){
+        if(words[i].length > longest)
+            longest = words[i].length
+    }
+    return longest;
+}
+quoteText = "Hello Doc!"
+var app = new Vue({
+    el: '#app',
+    data: {
+        quoteText: '',
+        length: length(quoteText),
+        punctuation: punctuation(quoteText),
+        longestWord: longestWord(quoteText),
+        caps: caps(quoteText),
+        classination: ''
+    },
+    methods: {
+        classinate: function() {
+            this.classination = waitMessage;
+            axios.post(classinateUrl, 
+                {   quoteText: this.quoteText,
+                    length,
+                    punctuation,
+                    longestWord,
+                    caps })
+                .then(response => {
+                    var reportClass = response.data.reportClass;
+                    this.classination = classMessages[reportClass];
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.classination = errorMessage;
+                });
+        }
+    }
+});
